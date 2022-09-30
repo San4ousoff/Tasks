4ac0aa761ca0cb0a110a6f06814a799d4e9dd07c
Kiriakov Aleksandr <san4ousoff@mail.ru>
2022-09-30 22:39:41 +0200

    add Homework 3

---
+++ Homework3/Program.cs
@@ -0,0 +1,15 @@
+﻿/* 
+Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
+(делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+Console.Clear();
+
+double number = new Random().Next(-100,100);
+//Console.WriteLine(number); - проверка ввода
+
+if (number % 2 == 0) Console.WriteLine("Число " + number + " чётное"); 
+else Console.WriteLine("Число " + number + " нечётное");
