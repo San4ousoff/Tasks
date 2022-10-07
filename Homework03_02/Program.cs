adb249a536fb73afc4cb9302db5c9573edf7f139
Kiriakov Aleksandr <san4ousoff@mail.ru>
2022-10-07 23:25:21 +0200

    add Homework lesson 03 task 02

---
+++ Homework03_02/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 
+3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+Console.Clear();
+
+Console.Write("Введите координату X точки А: ");
+int xA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату Y точки А: ");
+int yA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату Z точки А: ");
+int zA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату X точки B: ");
+int xB = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату Y точки B: ");
+int yB = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату Z точки B: ");
+int zB = int.Parse(Console.ReadLine()!);
+
+double result = Math.Sqrt(Math.Pow((xA-xB),2)+Math.Pow((yA-yB),2)+Math.Pow((zA-zB),2)); //формула нахождения
+                                                                                        //длины отрезка
+
+Console.WriteLine
+($"Длина отрезка АВ равна {Math.Round(result,2)}");
