4b22f62a00ec51c39f6c8560e6061ad787074bf6
Kiriakov Aleksandr <san4ousoff@mail.ru>
2022-10-07 22:23:25 +0200

    add Task 13

---
+++ Task13/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4
+*/
+Console.Clear();
+
+Console.Write("Введите простое число: ");
+int num = int.Parse(Console.ReadLine()!);
+int count = 1;
+
+Console.Write($"Таблица квадратов чисел: ");
+while (count<=num) 
+{
+    if (count<num) 
+    {
+        Console.Write(Math.Pow(count,2) + ", ");
+    }
+    else Console.Write(Math.Pow(count,2));
+    count++;
+};
