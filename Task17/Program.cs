1e7a0c8e5e2e32512caa45c89e7888473e18604a
Kiriakov Aleksandr <san4ousoff@mail.ru>
2022-10-11 20:17:39 +0200

    add Task17

---
+++ Task17/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая выводит массив из 8 
+элементов, заполненный нулями и единицами в случайном порядке. 
+[1,0,1,1,0,1,0,0]
+*/
+Console.Clear();
+
+int[]arr = new int[8];
+ArrRun(arr);
+
+void ArrRun (int[] arrTemp)
+{
+    for (int count=0; count<arr.Length; count++)
+    {
+        arr[count]=new Random().Next(0,2);
+        Console.Write($"{arr[count]} ");
+    }
+}
