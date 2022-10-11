0dd886cd90dd0c3260cd13ff016a7de177c759d3
Kiriakov Aleksandr <san4ousoff@mail.ru>
2022-10-11 19:47:46 +0200

    add Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Напишите программу, которая принимает на вход число и выдает сумму всех чисел от 1 до введённого.
+4 -> 1+2+3+4=10
+*/
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine ($"Сумма чисел = {GetSumNums(num)}"); // вызов метода
+
+int GetSumNums (int number) // метод суммирования
+{
+    int sum = 0;
+    while(number>0)
+    {
+        sum = sum + number;
+        number--;
+    }
+    return(sum);
+}
