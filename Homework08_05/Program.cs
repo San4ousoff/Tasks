e0969dd4c317aecf1ec28bb1af3fddf491b15b68
Kiriakov Aleksandr <san4ousoff@mail.ru>
2022-11-12 21:15:59 +0200

    add Homeworks lesson 8 task 5

--- Homework08_05/Program.cs
+++ Homework08_05/Program.cs
@@ -9,3 +9,43 @@
 */
 
 Console.Clear();
+
+Console.Write("Введите размер стороны квадратного массива: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int[,] snakeMatrix = GetMatrix (n);
+WriteArray(snakeMatrix);
+
+int[,] GetMatrix (int n)
+{
+    int[,] sqareMatrix = new int[n, n];
+    int temp = 1;
+    int i = 0;
+    int j = 0;
+
+    while (temp <= sqareMatrix.GetLength(0) * sqareMatrix.GetLength(1))
+    {
+        sqareMatrix[i, j] = temp;
+        temp++;
+        if (i <= j + 1 && i + j < sqareMatrix.GetLength(1) - 1) j++;
+    else if (i < j && i + j >= sqareMatrix.GetLength(0) - 1) i++;
+    else if (i >= j && i + j > sqareMatrix.GetLength(1) - 1) j--;
+    else i--;
+    }
+return sqareMatrix;
+}
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i,j] / 10 <= 0)
+      Console.Write($" {array[i,j]} ");
+
+      else Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}
